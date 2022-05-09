3898de843df95e7c7020078c284406edbd8f476c
Nan-Huo <NanHuo@users.noreply.github.com>
2022-05-09 20:11:06 +0800

    My CV
    My CV.

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-### Hack-hu 的个人简历
+# resume
 
-#### 个人简历
-[在线地址](https://hackhu2019.github.io/resume/)
-
-
+#### Description
+Nan HUO's CV.
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,3 @@
 window.onload = function(){
-	console.log("感谢您的浏览，期待能够加入贵公司！我的 GitHub：https://github.com/hackhu2019");
+	console.log("Thank you for your visit! My GitHub：https://github.com/Nan-Huo");
 }
